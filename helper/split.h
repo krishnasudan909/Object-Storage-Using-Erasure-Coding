adceb2b1801290a1a690f5ac9d063e63c7e705c4
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2022-11-11 13:09:19 +0530

    Changed the split function, made it compact and fast

--- helper/split.h
+++ helper/split.h
@@ -1,84 +1,27 @@
-
-int splitFile(char *fileIn, size_t maxSize)
-{
-    int result = 0;
+int splitFile(char *fileIn, size_t maxSize){
+    int result = 1;
     FILE *fIn;
     FILE *fOut;
-    char buffer[1024 * 16];
+    char buffer[maxSize];
     char dirname[3];
+    char filename[50];
     size_t size;
     size_t read;
     size_t written;
 
 
-    if ((fileIn != NULL) && (maxSize > 0))
-    {
-        fIn = fopen(fileIn, "rb");
-        if (fIn != NULL)
-        {
-            fileIn = fullName(fileIn);
-            fOut = NULL;
-            result = 1;   /* we have at least one part */
+    fIn = fopen(fileIn, "rb");
+    fOut = NULL;
+    while (!feof(fIn)){
+        sprintf(dirname, "d%d", result);
+        mkdir(dirname,0777);
+        sprintf(filename, "%s/%s.%03d", dirname, fileIn, result);
+        fOut = fopen(filename, "wb");
 
-            while (!feof(fIn))
-            {
-                /* initialize (next) output file if no output file opened */
-                if (fOut == NULL)
-                {
-                    sprintf(dirname, "d%d", result);
-                    mkdir(dirname,0777);
-                    sprintf(buffer, "%s/%s.%03d", dirname, fileIn, result);
-                    fOut = fopen(buffer, "wb");
-                    if (fOut == NULL)
-                    {
-                        result *= -1;
-                        break;
-                    }
-
-                    size = 0;
-                }
-
-                
-                read = sizeof(buffer);
-                if ((size + read) > maxSize)
-                {
-                    read = maxSize - size;
-                }
-
-                
-                read = fread(buffer, 1, read, fIn);
-                if (read == 0)
-                {
-                    result *= -1;
-                    break;
-                }
-
-                
-                written = fwrite(buffer, 1, read, fOut);
-                if (written != read)
-                {
-                    result *= -1;
-                    break;
-                }
-
-               
-                size += written;
-                if (size >= maxSize)   
-                {
-                    fclose(fOut);
-                    fOut = NULL;
-                    result++;
-                }
-            }
-
-            /* clean up */
-            if (fOut != NULL)
-            {
-                fclose(fOut);
-            }
-            fclose(fIn);
-        }
+        fread(buffer, maxSize, 1, fIn);
+        fwrite(buffer, maxSize, 1, fOut );
+        fclose(fOut);
+        result++;
     }
-
-    return (result);
+    return result;
 }
