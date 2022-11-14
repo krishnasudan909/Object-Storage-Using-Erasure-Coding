7b2226b31bd516c4234884cb5e22f2fc57f54f98
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2022-11-14 13:33:18 +0530

    commit to remove problems in pushing

--- helper/merge.h
+++ helper/merge.h
@@ -11,7 +11,7 @@ void mergeFile(char *filename, size_t splitSize){
     for (int i = 1; i <= 4; i++)
     {
         sprintf(splitlocation, "d%d/%s.00%d", i, filename, i);
-        printf("%s\n", splitlocation);
+        // printf("%s\n", splitlocation);
         fIn = fopen(splitlocation, "rb");
         fread(buffer, splitSize, 1, fIn);
         // printf("%s\n", buffer);
